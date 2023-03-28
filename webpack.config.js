2bfc016c04cb282214196f6dc86d011bd21788b0
Jorman Urbina <jormanurbina1@gmail.com>
2023-03-28 13:14:38 -0300

    se actualizo webpack.config

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+//https://webpack.js.org/guides/getting-started/#using-a-configuration
+
+// El archivo de configuración nos va ayudar a poder establecer la configuración y elementos que vamos a utilizar
+// Para poder crear el archivo de configuración en la raíz del proyecto creamos un archivo llamado webpack.config.js
+// En el mismo debemos decir
+// El punto de entrada
+// Hacia a donde a enviar la configuración de nuestro proyecto
+// Las extensiones que vamos usar
+
+const path = require('path');
+
+module.exports = {
+  // Entry nos permite decir el punto de entrada de nuestra aplicación
+  entry: "./src/index.js",
+  // Output nos permite decir hacia dónde va enviar lo que va a preparar webpacks
+  output: {
+    // path es donde estará la carpeta donde se guardará los archivos
+    // Con path.resolve podemos decir dónde va estar la carpeta y la ubicación del mismo
+    path: path.resolve(__dirname, "dist"),
+    // filename le pone el nombre al archivo final
+    filename: "main.js"
+  },
+  resolve: {
+    // Aqui ponemos las extensiones que tendremos en nuestro proyecto para webpack los lea
+    extensions: [".js"]
+  },
+}
