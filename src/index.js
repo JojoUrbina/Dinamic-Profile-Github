f6c8486f648b0911ff66f5d247bed606a63f0f4d
Jorman Urbina <jormanurbina1@gmail.com>
2023-03-30 22:21:16 -0300

    se crearon alias a las rutas para mejorar la lectura de las mismas

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import Template from './templates/Template.js';
-import "./styles/main.css";//se debe agregar aca para poder visualizar ls estilos
-import "./styles/vars.styl";//se instalo , luego se agrego en el webpack config
+import Template from '@templates/Template.js';
+import "@styles/main.css";//se debe agregar aca para poder visualizar ls estilos
+import "@styles/vars.styl";//se instalo , luego se agrego en el webpack config
 
 (async function App() {
   const main = null || document.getElementById('main');
--- webpack.config.js
+++ webpack.config.js
@@ -28,7 +28,13 @@ module.exports = {
   },
   resolve: {
     // Aqui ponemos las extensiones que tendremos en nuestro proyecto para webpack los lea
-    extensions: [".js"]
+    extensions: [".js"],
+    alias:{
+      "@utils":path.resolve(__dirname,"src/utils/"),
+      "@templates":path.resolve(__dirname,"src/templates/"),
+      "@styles":path.resolve(__dirname,"src/styles/"),
+      "@images":path.resolve(__dirname,"src/assets/images/"),
+    }
   },
   module:{
     rules:[
@@ -59,7 +65,7 @@ module.exports = {
         mimetype:"aplicatio/fotn-woff",
         name:"[name].[contenthash].[ext]",
         outputPath:"./assets/fonts/",
-        publicPath:"./assets/fonts/",
+        publicPath:"../assets/fonts/",
         esModule:false,
 
       }
