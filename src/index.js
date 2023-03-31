fea0295659dbc25dc5f8739d637722a80881f1a3
Jorman Urbina <jormanurbina1@gmail.com>
2023-03-31 18:03:39 -0300

    se creo archivo metlify para hacer deploy del proyecto

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import Template from '@templates/Template.js';
 import "@styles/main.css";//se debe agregar aca para poder visualizar ls estilos
 import "@styles/vars.styl";//se instalo , luego se agrego en el webpack config
-
+console.log("hosla")
 (async function App() {
   const main = null || document.getElementById('main');
   main.innerHTML = await Template();
